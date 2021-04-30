3edcb0859c28a4365096bd0fc7fbb35bd1b6ea4f
PronabM <pronab.mukherjee.96@gmail.com>
2021-04-30 23:26:51 +0530

    Improved Coverage

--- src/main/java/battery/CurrentRangeMonitor.java
+++ src/main/java/battery/CurrentRangeMonitor.java
@@ -11,6 +11,7 @@ import java.util.function.Predicate;
 
 public final class CurrentRangeMonitor 
 {
+	private CurrentRangeMonitor() {}
 	
 	@SafeVarargs
 	private static <T> Predicate<T> atleastOne(Predicate<T> ... predicates) {
